e7f18b10b20570ba5491d5616d00d651087df2b2
Rodrigo Ernesto Álvarez Aguilera <incognia@gmail.com>
2019-09-15 03:41:10 -0500

    Permisos de ejecución y archivo para Apache

--- apache-bkp.sh
+++ apache-bkp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#   wp-bkp.sh - Respaldo del sistema de gestion de contenidos WordPress
+#   apache-bkp.sh - Respaldo del sistema de gestion de contenidos WordPress
 #
 #   Copyright © 2019, Rodrigo Ernesto Alvarez Aguilera <incognia@gmail.com>
 #
@@ -16,31 +16,29 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#   Version 0.2 - May 13, 2019
+#   Version 0.1 - September 15, 2019
 #   Author: Rodrigo Ernesto Alvarez Aguilera
 #
 #   Tested under Debian GNU/Linux 9.7 using GNU bash version 4.4.12
 #
 
-HELP="Uso: wp-bkp.sh [OPCIÓN]\n\n
+HELP="Uso: apache-bkp.sh [OPCIÓN]\n\n
 \t-h\tmuestra esta lista de ayuda\n
 \t-l\tmuestra la licencia del programa\n
 \t-v\tmuestra la versión del programa\n"
 
-VERSION="wp-bkp.sh versión 0.2\n
+VERSION="apache-bkp.sh versión 0.2\n
 \bCopyright © 2019, Rodrigo Ernesto Alvarez Aguilera"
-
-DATABASE="wordpress"
-
-DBUSER=[database user]
-
-DBPASS=[dabase password]
 
 DATE=`date +%Y%m%d`
 
 HOST=`hostname`
 
-NFS=[nfs server]
+DATABASE=[database name]
+
+DBUSER=[database user]
+
+DBPASS=[dabase password]
 
 if  [[ $1 = "-h" ]]; then
     echo -e $HELP
@@ -49,22 +47,14 @@ elif  [[ $1 = "-l" ]]; then
 elif  [[ $1 = "-v" ]]; then
     echo -e $VERSION
 else
-    # Matar procesos usando directorio NFS
-    fuser -k -9 /var/www/html/archivos/
-    # Desmontar directorio NFS
-    umount /var/www/html/archivos/
-    df -h
-    sleep 5
     # Respaldar y comprimir directorio "html"
     tar -czvf $DATE-$HOST-html.tar.gz /var/www/html/
     # Respaldar base de datos
     mysqldump -u $DBUSER -p$DBPASS $DATABASE > $DATABASE.sql
     # Comprimir respaldo de base de datos
-    tar -czvf $DATE-$HOST-wordpress.tar.gz wordpress.sql
+    tar -czvf $DATE-$HOST-$DATABASE.tar.gz $DATABASE.sql
     # Eliminar respaldo de base de datos
-    rm wordpress.sql
-    # Montar directorio NFS
-    mount -t nfs $NFS:/data/archivos /var/www/html/archivos/
+    rm $DATABASE.sql
     echo -e "┌──────────────────────────────────────────────────────────────────────────────┐"
     echo -e "│                            Generando informe...                              │"
     echo -e "└──────────────────────────────────────────────────────────────────────────────┘"
